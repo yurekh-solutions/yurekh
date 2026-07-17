200df6466ab6a1fe85fc3db4f3ea2943015b6e6b
Yureks Solutions <yureksolutions@gmail.com>
2026-07-17 18:15:45 +0530

    Remove temporary cleanup scripts

--- copy_og.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const src = 'C:/Users/yurek/.qoder/vibe_images/yurekh-og-image_1784291618.png';
-const dest = path.join(__dirname, 'public', 'og-image.png');
-fs.copyFileSync(src, dest);
-console.log('Copied to', dest);
-console.log('Size:', fs.statSync(dest).size, 'bytes');
